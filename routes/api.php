436547b253c8ca3b41220821ac7c037c5d1cd34b
kozimatakesi <riverbook708@gmail.com>
2022-10-18 23:49:08 +0900

    add API

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('/tasks', 'TaskController@index');
+Route::post('/tasks', 'TaskController@store');
+Route::get('/tasks/{task}', 'TaskController@show');
+Route::put('/tasks/{task}', 'TaskController@update');
+Route::delete('/tasks/{task}', 'TaskController@destroy');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
